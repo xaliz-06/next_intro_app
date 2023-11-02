3343e762a37775f921905c0538689e9205b4951e
xaliz-06 <vaibhavchakraborty06@gmail.com>
2023-11-02 17:09:16 +0530

    fixed fallback

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -42,7 +42,7 @@ export async function getStaticPaths() {
   client.close();
 
   return {
-    fallback: false,
+    fallback: "blocking",
     paths: meetups.map((meetup) => ({
       params: { meetupId: meetup._id.toString() },
     })),
